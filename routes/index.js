72693bc4006ad9484a5f2c547d2ffb5aebc1f127
Anuraj Jit Saikia <anurajjit@gmail.com>
2025-05-19 10:08:49 +0530

    Bugs Fixed

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Import route modules
-const apiRoutes = require('./api');
-const assessmentRoutes = require('./assessment');
-const pageRoutes = require('./pages');
-const searchRoutes = require('./search');
+const apiRoutes = require("./api");
+const assessmentRoutes = require("./assessment");
+const pageRoutes = require("./pages");
+const searchRoutes = require("./search");
 
 // Use route modules
-router.use('/api', apiRoutes);
-router.use('/assessment', assessmentRoutes);
-router.use('/search', searchRoutes);
-router.use('/', pageRoutes);
+router.use("/api", apiRoutes);
+router.use("/assessment", assessmentRoutes);
+router.use("/search", searchRoutes);
+router.use("/", pageRoutes);
 
 // Special case for the legacy /test endpoint which should map to assessmentRoutes
-router.post('/test', (req, res) => {
-  const origUrl = req.url;
-  req.url = '/submit';
-  assessmentRoutes(req, res);
+router.post("/test", (req, res, next) => {
+  // Redirect to the assessment submit route
+  res.redirect(307, "/assessment/submit");
 });
 
 // Special case for /career-paths to maintain compatibility
-router.get('/career-paths', (req, res) => {
-  const origUrl = req.url;
-  req.url = '/assessment/career-paths';
-  router.handle(req, res);
+router.get("/career-paths", (req, res, next) => {
+  // Redirect to the assessment career-paths route
+  res.redirect("/assessment/career-paths");
 });
 
-module.exports = router; 
+module.exports = router;
